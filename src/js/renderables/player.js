6c8c51561e2b0df2588e29cc5561df56c7566678
Udit Vira <udit.vira@gmail.com>
2022-04-16 19:41:26 -0400

    try openrelay turn server

--- src/js/renderables/player.js
+++ src/js/renderables/player.js
@@ -51,6 +51,28 @@ class PlayerEntity extends PlayerWithLabelAndMediaEntity {
       path: '/myapp',
       secure: true,
       debug: 3, // errors
+      config: {
+        iceServers: [
+          {
+            urls: "stun:openrelay.metered.ca:80",
+          },
+          {
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          },
+          {
+            urls: "turn:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          },
+          {
+            urls: "turn:openrelay.metered.ca:443?transport=tcp",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          },
+        ]
+      }
     })
     var that = this
 
